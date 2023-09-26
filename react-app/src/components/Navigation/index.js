0fc79ac94cf73c426df38d73bcce451b0d842af9
joshjk <jokrienke@comcast.net>
2023-09-25 21:57:46 -0700

    dienda

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -15,8 +15,28 @@ function Navigation({ isLoaded }){
 				<NavLink exact to="/"><img className='home-button' src='https://cdn.discordapp.com/attachments/1115823811116400650/1153915198898450462/joshisgay3.png' alt='home button'></img></NavLink>
 			</li>
 			<li className='search-bar'>
-				<input className='search-input' type='text' onChange={e => setQuery(e.target.value)} placeholder='Search...'></input>
-					<i class='fa fa-search'></i>
+				<form className='search-bar-form'>
+					<input className='search-input' type='text' onChange={e => setQuery(e.target.value)} placeholder='Search...'></input>
+					<select className='genre-input-1'>
+						<option value='' disabled selected>Genre</option>
+						<option value="0">Mexican</option>
+                		<option value="1">Korean</option>
+                		<option value="2">American</option>
+                		<option value="3">Japanese</option>
+                		<option value="4">French</option>
+                		<option value="5">Indian</option>
+					</select>
+					<select className='price-input-1'>
+						<option value='' disabled selected>Price</option>
+						<option value="0">$</option>
+                		<option value="1">$$</option>
+                		<option value="2">$$$</option>
+                		<option value="3">$$$$</option>
+					</select>
+					 <button class="search-submit-button">
+						<i class='fa fa-search'></i>
+					</button>
+				</form>
 			</li>
 			{isLoaded && (
 				<li>
