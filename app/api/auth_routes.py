fbcaa6719fc40aa1f0d2b37f96a6e37047503392
joshjk <jokrienke@comcast.net>
2023-11-07 21:22:42 -0800

    nav-bar-greenlit

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -8,9 +8,7 @@ auth_routes = Blueprint('auth', __name__)
 
 
 def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
+
     errorMessages = []
     for field in validation_errors:
         for error in validation_errors[field]:
@@ -20,9 +18,7 @@ def validation_errors_to_error_messages(validation_errors):
 
 @auth_routes.route('/')
 def authenticate():
-    """
-    Authenticates a user.
-    """
+
     if current_user.is_authenticated:
         return current_user.to_dict()
     return {'errors': ['Unauthorized']}
@@ -30,15 +26,10 @@ def authenticate():
 
 @auth_routes.route('/login', methods=['POST'])
 def login():
-    """
-    Logs a user in
-    """
+
     form = LoginForm()
-    # Get the csrf_token from the request cookie and put it into the
-    # form manually to validate_on_submit can be used
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        # Add the user to the session, we are logged in!
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
         return user.to_dict()
@@ -47,18 +38,12 @@ def login():
 
 @auth_routes.route('/logout')
 def logout():
-    """
-    Logs a user out
-    """
     logout_user()
     return {'message': 'User logged out'}
 
 
 @auth_routes.route('/signup', methods=['POST'])
 def sign_up():
-    """
-    Creates a new user and logs them in
-    """
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
@@ -76,7 +61,4 @@ def sign_up():
 
 @auth_routes.route('/unauthorized')
 def unauthorized():
-    """
-    Returns unauthorized JSON when flask-login authentication fails
-    """
     return {'errors': ['Unauthorized']}, 401
