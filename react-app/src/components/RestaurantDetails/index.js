3e57cd008a4a77825513f9cb08ad82b435fe356c
JustAMan22 <irriatingrobloxgamers@yahoo.com>
2023-11-11 17:41:30 -0600

    Added vignette to restaurant detail, and fxied some css

--- react-app/src/components/RestaurantDetails/index.js
+++ react-app/src/components/RestaurantDetails/index.js
@@ -11,124 +11,219 @@ import ImagesModalButton from "../OpenModalButton/indexv3";
 import CreateRestaurantImage from "../createResImagesForm";
 
 function RestaurantDetailsPage() {
-  const history = useHistory()
-  const dispatch = useDispatch();
-  const { restaurantId } = useParams();
-  const restaurant = useSelector((state) => state?.restaurant[restaurantId]);
-  const sessionUser = useSelector((state) => state?.session.user);
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const { restaurantId } = useParams();
+	const restaurant = useSelector((state) => state?.restaurant[restaurantId]);
+	const sessionUser = useSelector((state) => state?.session.user);
 
+	useEffect(() => {
+		dispatch(loadRestaurantDetails(restaurantId));
+	}, [dispatch, restaurantId]);
 
-  useEffect(() => {
-    dispatch(loadRestaurantDetails(restaurantId));
-  }, [dispatch, restaurantId]);
+	const images = [];
 
-  const images = [];
+	if (restaurant?.images.length) {
+		restaurant?.images.forEach((image) => {
+			images.push(image.url);
+		});
+	}
+	if (restaurant?.reviews.length) {
+		restaurant?.reviews.forEach((review) => {
+			if (review?.images.length) {
+				review?.images.forEach((image) => {
+					images.push(image.url);
+				});
+			}
+		});
+	}
 
-  if (restaurant?.images.length) {
-    restaurant?.images.forEach(image => {
-      images.push(image.url)
-    });
-  }
-  if (restaurant?.reviews.length) {
-    restaurant?.reviews.forEach((review) => {
-      if (review?.images.length) {
-        review?.images.forEach(image => {
-          images.push(image.url)
-        })
-      }
-    })
-  }
+	let priceFunc;
+	const price = restaurant?.price;
+	if (price === 4) {
+		priceFunc = "$$$$";
+	} else if (price === 3) {
+		priceFunc = "$$$";
+	} else if (price === 2) {
+		priceFunc = "$$";
+	} else if (price === 1) {
+		priceFunc = "$";
+	}
 
-  let priceFunc;
-  const price = restaurant?.price
-  if (price == 4) {
-    priceFunc = "$$$$"
-  } else if (price == 3) {
-    priceFunc = "$$$"
-  } else if (price == 2) {
-    priceFunc = "$$"
-  } else if (price == 1) {
-    priceFunc = "$"
-  }
+	return (
+		<div className="detail-body">
+			<div id="res-img">
+				{restaurant?.images.length > 0 && (
+					<img
+						className="img-tile"
+						src={restaurant?.images[0]?.url}
+					/>
+				)}
+				<div className="info-thing">
+					<div id="restaurant-name"> {restaurant?.name}</div>
+					{restaurant?.reviews?.length ? (
+						<div className="rating-div">
+							<div className="stars-div">
+								{[...Array(Math.floor(restaurant?.rating))].map(
+									(_, i) => (
+										<i
+											key={i}
+											id="review-star-detail"
+											className="fa-solid fa-star"
+										></i>
+									)
+								)}
 
-  return (
-    <div className="detail-body">
-      <div id="res-img">
-        {restaurant?.images.length > 0 && (
-          <img className="img-tile" src={restaurant?.images[0]?.url} />
-        )}
-        <div className="info-thing">
-          <div id="restaurant-name"> {restaurant?.name}</div>
-          <div id="restaurant-rtng">Rating: {restaurant?.rating} ({restaurant?.reviews.length} reviews)</div>
-          <div id="restaurant-cat">
-            <div id="restaurant-price">
-              {priceFunc} · {restaurant?.category}
-            </div>
-            <ImagesModalButton
-              buttonText="See all photos"
-              modalComponent={<ImagesFormModal images={images} name={restaurant?.name} />} />
-          </div>
-        </div>
-      </div>
-      <div id="res-details">
-        <div id="option-buttons">
-          {sessionUser?.id !== restaurant?.owner.id && sessionUser?.id && (
-            <button onClick={() => history.push(`/restaurants/${restaurant?.id}/new-review`)}>Write a review</button>
-          )}
-          {sessionUser?.id === restaurant?.owner?.id && (
-            <div id="manage-buttons">
-              <button onClick={() => history.push(`/restaurants/edit/${restaurant?.id}`)}>Update Restaurant Info</button>
-              <DetailsModalButton
-                buttonText="Add Photos"
-                modalComponent={<CreateRestaurantImage restaurantId={restaurant?.id} />}
-              />
-              <DetailsModalButton
-                buttonText="Delete Restaurant"
-                modalComponent={<DeleteForm restaurantId={restaurant?.id} />}
-              />
-            </div>
-          )}
-          <div id="res-location">
-            <div id="location-tag">Location:</div>
-            <div> {restaurant?.address}, {restaurant?.city}, {restaurant?.state}, {restaurant?.country}</div>
-          </div>
-        </div>
-        <div id="all-da-reviews">
-          <div id="rec-review-text">Recommended Reviews</div>
-          {restaurant?.reviews?.map((review) => (
-            <div id="res-details-rev-info">
-              <div id="rev-prof-header">
-                <div id="prof-stars">
-                  <span id='profile-icon' class="material-symbols-outlined">person</span>
-                  <div id="reviewer-username">
-                    {review?.reviewer?.username}
-                    <div>{[...Array(review?.stars)].map((_, i) => <span key={i} class="material-symbols-outlined">star_rate</span>)}</div>
-                  </div>
-                </div>
-                {review?.reviewer?.id === sessionUser?.id && (
-                  <div id="manage-rev-buttons">
-                    <button onClick={() => history.push(`/restaurants/${restaurant?.id}/review/${review?.id}/edit`)}>Update Review</button>
-                    <DetailsModalButton
-                      buttonText="Delete Review"
-                      modalComponent={<DeleteReviewForm reviewId={review?.id} restaurantId={restaurant?.id} />}
-                    />
-                  </div>
-                )}
-              </div>
-              <div id="details-review-text">{review?.review}</div>
-              {review?.images.length > 0 && (
-                <div id="rev-imgs-container">
-                  {review.images.map((image) => (
-                    <img src={image.url} />
-                  ))}
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+								{restaurant?.rating % 1 >= 0.3 &&
+									restaurant?.rating % 1 <= 0.7 && (
+										<i
+											id="review-star-half-detail"
+											className="fa-solid fa-star"
+										></i>
+									)}
+							</div>
+
+							<div className="rating-text-detail">
+								{restaurant?.rating}{" "}
+								<span id="num-reviews-text-detail">
+									({restaurant?.reviews?.length} reviews)
+								</span>
+							</div>
+						</div>
+					) : null}
+					<div id="restaurant-cat">
+						<div id="restaurant-price">
+							{priceFunc} · {restaurant?.category}
+						</div>
+						<ImagesModalButton
+							buttonText="See all photos"
+							modalComponent={
+								<ImagesFormModal
+									images={images}
+									name={restaurant?.name}
+								/>
+							}
+						/>
+					</div>
+				</div>
+			</div>
+			<div id="res-details">
+				<div id="option-buttons">
+					{sessionUser?.id !== restaurant?.owner.id &&
+						sessionUser?.id && (
+							<button
+								onClick={() =>
+									history.push(
+										`/restaurants/${restaurant?.id}/new-review`
+									)
+								}
+							>
+								Write a review
+							</button>
+						)}
+					{sessionUser?.id === restaurant?.owner?.id && (
+						<div id="manage-buttons">
+							<button
+								onClick={() =>
+									history.push(
+										`/restaurants/edit/${restaurant?.id}`
+									)
+								}
+							>
+								Update Restaurant Info
+							</button>
+							<DetailsModalButton
+								buttonText="Add Photos"
+								modalComponent={
+									<CreateRestaurantImage
+										restaurantId={restaurant?.id}
+									/>
+								}
+							/>
+							<DetailsModalButton
+								buttonText="Delete Restaurant"
+								modalComponent={
+									<DeleteForm restaurantId={restaurant?.id} />
+								}
+							/>
+						</div>
+					)}
+					<div id="res-location">
+						<div id="location-tag">Location:</div>
+						<div>
+							{" "}
+							{restaurant?.address}, {restaurant?.city},{" "}
+							{restaurant?.state}, {restaurant?.country}
+						</div>
+					</div>
+				</div>
+				<div id="all-da-reviews">
+					<div id="rec-review-text">Recommended Reviews</div>
+					{restaurant?.reviews?.map((review) => (
+						<div id="res-details-rev-info">
+							<div id="rev-prof-header">
+								<div id="prof-stars">
+									<span
+										id="profile-icon"
+										class="material-symbols-outlined"
+									>
+										person
+									</span>
+									<div id="reviewer-username">
+										{review?.reviewer?.username}
+										<div>
+											{[...Array(review?.stars)].map(
+												(_, i) => (
+													<span
+														key={i}
+														class="material-symbols-outlined"
+													>
+														star_rate
+													</span>
+												)
+											)}
+										</div>
+									</div>
+								</div>
+								{review?.reviewer?.id === sessionUser?.id && (
+									<div id="manage-rev-buttons">
+										<button
+											onClick={() =>
+												history.push(
+													`/restaurants/${restaurant?.id}/review/${review?.id}/edit`
+												)
+											}
+										>
+											Update Review
+										</button>
+										<DetailsModalButton
+											buttonText="Delete Review"
+											modalComponent={
+												<DeleteReviewForm
+													reviewId={review?.id}
+													restaurantId={
+														restaurant?.id
+													}
+												/>
+											}
+										/>
+									</div>
+								)}
+							</div>
+							<div id="details-review-text">{review?.review}</div>
+							{review?.images.length > 0 && (
+								<div id="rev-imgs-container">
+									{review.images.map((image) => (
+										<img src={image.url} />
+									))}
+								</div>
+							)}
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default RestaurantDetailsPage;
