ded05910556d5fe25ce892ba292f9f2b8986e0c1
JustAMan22 <irriatingrobloxgamers@yahoo.com>
2023-12-05 21:49:43 -0600

    Revamped homepage tiles, and review create/update

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,120 +1,208 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, NavLink, Switch } from 'react-router-dom';
+import { useParams, NavLink, Switch } from "react-router-dom";
 import { fetchReviews } from "../../store/reviews";
 import { loadRestaurants } from "../../store/restaurants";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import "./HomePage.css";
 
-
 const HomePage = () => {
+	const [stars, setStars] = useState();
+	function getRandomNumbers(maxNumber) {
+		const allNumbers = Array.from(
+			{ length: maxNumber },
+			(_, index) => index + 1
+		);
 
+		for (let i = allNumbers.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[allNumbers[i], allNumbers[j]] = [allNumbers[j], allNumbers[i]];
+		}
 
+		return allNumbers.slice(0, 9);
+	}
 
-    function getRandomNumbers(maxNumber) {
-        const allNumbers = Array.from({ length: maxNumber }, (_, index) => index + 1);
+	const dispatch = useDispatch();
+	let restaurantObject = useSelector((state) => state.restaurant);
+	let restaurantArray = Object.values(restaurantObject);
+	let restaurantIterable = restaurantArray.length;
+	let reviewObject = useSelector((state) => state.reviews);
+	let reviewArray = Object.values(reviewObject);
+	let iterable = reviewArray.length;
 
-        for (let i = allNumbers.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [allNumbers[i], allNumbers[j]] = [allNumbers[j], allNumbers[i]];
-        }
+	let reviewId = getRandomNumbers(iterable);
+	let restaurantId = getRandomNumbers(restaurantIterable);
 
-        return allNumbers.slice(0, 9);
-    }
+	let array = [];
+	reviewId.forEach((id) => {
+		array.push(reviewArray[id - 1]);
+	});
 
+	let arr = [];
+	restaurantId.forEach((id) => {
+		arr.push(restaurantArray[id - 1]);
+	});
 
-    const dispatch = useDispatch();
-    let restaurantObject = useSelector((state) => state.restaurant)
-    let restaurantArray = Object.values(restaurantObject)
-    let restaurantIterable = restaurantArray.length
-    let reviewObject = useSelector((state) => state.reviews)
-    let reviewArray = Object.values(reviewObject)
-    let iterable = reviewArray.length
+	useEffect(() => {
+		dispatch(fetchReviews());
+		dispatch(loadRestaurants(0, 0, 0));
+	}, [dispatch]);
+	/* <img src={review.images[0].url} /> */
 
+	// reviewObject = Object.values(reviewObject)
 
+	const getTooltip = (star) => {
+		switch (star) {
+			case 1:
+				return "Not Good";
+			case 2:
+				return "Could've been better";
+			case 3:
+				return "OK";
+			case 4:
+				return "Good";
+			case 5:
+				return "Great";
+			default:
+				return "";
+		}
+	};
 
+	const recommendationTexts = [
+		"Do you recommend this business?",
+		"Share what was great about it",
+		"Help the community decide",
+	];
 
-    let reviewId = getRandomNumbers(iterable)
-    let restaurantId = getRandomNumbers(restaurantIterable)
-
-
-    let array = [];
-    reviewId.forEach(id => {
-        array.push(reviewArray[id - 1])
-    })
-
-    let arr = [];
-    restaurantId.forEach(id => {
-        arr.push(restaurantArray[id - 1])
-    })
-
-
-
-
-
-    useEffect(() => {
-        dispatch(fetchReviews())
-        dispatch(loadRestaurants(0, 0, 0))
-    }, [dispatch])
-    /* <img src={review.images[0].url} /> */
-
-
-    // reviewObject = Object.values(reviewObject)
-
-
-
-    return (
-        <>
-            <div id="backround-image"></div>
-            <div className="restaurant-container">
-                <h2 id="header-text">Your Next Review Awaits</h2>
-                <ul className="restaurant-wrapper">
-                    {arr?.map((restaurant) => (
-                        <NavLink id="restaurant-link" to={`/restaurants/${restaurant?.id}`}>
-                            <li className="single-restaurant">
-                                <img className="rst-img-home" src={restaurant?.images[0]?.url} />
-                                <h2>{restaurant?.name}<p>Do you reccomend this business?</p></h2>
-                            </li>
-                        </NavLink>
-                    ))}
-                </ul>
-            </div>
-            <div id="reviews-wrapper">
-                <h2>Recent Activity</h2>
-                <ul className="review-wrapper">
-                    {array?.map((review) => (
-                        <NavLink id="homepage-review" to={`/restaurants/${review?.restaurant_id}`}>
-                            <li key={review?.id} className="single-review">
-                                <div id="profile-header">
-                                    <span id="prof-picture" class="material-symbols-outlined">account_circle</span>
-                                    <div id="container-username-text" >
-                                        <div id="reviewer-username">{review?.reviewer?.username}</div>
-                                        <div id="filler-text-review">Left a review...</div>
-                                    </div>
-                                </div>
-                                {review?.images?.map((image) => (
-                                    <img className="homepage-image" src={image?.url} alt='review-image' key={image?.id}></img>
-                                ))}
-                                {restaurantArray?.map((restaurant) => (
-                                    <div id="rst-name-in-review" key={restaurant?.id}>
-                                        {restaurant?.id === review?.restaurant_id ? restaurant?.name : ""}
-                                    </div>
-                                ))}
-                                <div id="reviewer-stars">
-                                    {[...Array(review?.stars)].map((_, i) => <i id="review-star-homepage" className="fa-solid fa-star"></i>)}
-                                </div>
-                                <div id="reviewer-review">{review?.review}</div>
-                            </li>
-                        </NavLink>
-                    ))}
-
-                </ul>
-            </div>
-        </>
-    )
-
-
-
-}
+	return (
+		<>
+			<div id="backround-image"></div>
+			<div className="restaurant-container">
+				<h2 id="header-text">Your Next Review Awaits</h2>
+				<div className="restaurant-wrapper">
+					{arr?.map((restaurant) => (
+						<div className="rec-res-container">
+							<div className="single-restaurant">
+								<div className="rec-img-container">
+									<img
+										className="rst-img-home"
+										src={restaurant?.images[0]?.url}
+									/>
+								</div>
+								<div className="rec-info-container">
+									<NavLink
+										id="restaurant-link"
+										to={`/restaurants/${restaurant?.id}`}
+									>
+										<div id="rec-res-name">
+											{restaurant?.name.length >= 21
+												? restaurant.name.slice(0, 21) +
+												  "..."
+												: restaurant?.name}
+										</div>
+									</NavLink>
+									<div>
+										{
+											recommendationTexts[
+												Math.floor(
+													Math.random() *
+														recommendationTexts.length
+												)
+											]
+										}
+									</div>
+									<NavLink
+										id="rec-rating-navlink"
+										to={`/restaurants/${restaurant?.id}/new-review`}
+									>
+										<div className="stars-rec">
+											{[5, 4, 3, 2, 1].map((star) => (
+												<div
+													key={star}
+													className={`star-rec ${
+														star <= stars
+															? "filled-rec"
+															: ""
+													}`}
+													onClick={() =>
+														setStars(star)
+													}
+													title={getTooltip(star)}
+												>
+													<i
+														id="review-star-homepage-rec"
+														className="fa-solid fa-star"
+													></i>
+												</div>
+											))}
+										</div>
+									</NavLink>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div id="reviews-wrapper">
+				<h2>Recent Activity</h2>
+				<ul className="review-wrapper">
+					{array?.map((review) => (
+						<NavLink
+							id="homepage-review"
+							to={`/restaurants/${review?.restaurant_id}`}
+						>
+							<li key={review?.id} className="single-review">
+								<div id="profile-header">
+									<span
+										id="prof-picture"
+										class="material-symbols-outlined"
+									>
+										account_circle
+									</span>
+									<div id="container-username-text">
+										<div id="reviewer-username">
+											{review?.reviewer?.username}
+										</div>
+										<div id="filler-text-review">
+											Left a review...
+										</div>
+									</div>
+								</div>
+								{review?.images?.map((image) => (
+									<img
+										className="homepage-image"
+										src={image?.url}
+										alt="review-image"
+										key={image?.id}
+									></img>
+								))}
+								{restaurantArray?.map((restaurant) => (
+									<div
+										id="rst-name-in-review"
+										key={restaurant?.id}
+									>
+										{restaurant?.id ===
+										review?.restaurant_id
+											? restaurant?.name
+											: ""}
+									</div>
+								))}
+								<div id="reviewer-stars">
+									{[...Array(review?.stars)].map((_, i) => (
+										<i
+											id="review-star-homepage"
+											className="fa-solid fa-star"
+										></i>
+									))}
+								</div>
+								<div id="reviewer-review">{review?.review}</div>
+							</li>
+						</NavLink>
+					))}
+				</ul>
+			</div>
+		</>
+	);
+};
 
 export default HomePage;
--- react-app/src/components/UpdateReview/index.js
+++ react-app/src/components/UpdateReview/index.js
@@ -5,127 +5,136 @@ import { updateReview, fetchReviews } from "../../store/reviews";
 import "../UpdateReview/UpdateReview.css";
 
 function UpdateReviewFunc() {
-  const dispatch = useDispatch();
-  const { restaurantId, reviewId } = useParams();
-  const currentReview = useSelector((state) => state?.reviews[reviewId]);
-  const history = useHistory();
-  const [review, setReview] = useState(currentReview?.review);
-  const [stars, setStars] = useState(currentReview?.stars);
-  const [errors, setErrors] = useState({});
-  const [submitted, setSubmitted] = useState(false);
+	const dispatch = useDispatch();
+	const { restaurantId, reviewId } = useParams();
+	const currentReview = useSelector((state) => state?.reviews[reviewId]);
+	const history = useHistory();
+	const [review, setReview] = useState(currentReview?.review);
+	const [stars, setStars] = useState(currentReview?.stars);
+	const [errors, setErrors] = useState({});
+	const [submitted, setSubmitted] = useState(false);
 
-  useEffect(() => {
-    dispatch(fetchReviews());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchReviews());
+	}, [dispatch]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const errors = {};
-    if (review.length < 10)
-      errors.review = "Review text must be greater than 10 characters.";
-    if (review.length > 250)
-      errors.review = "Review text must be 250 characters or less.";
-    if (!stars) errors.stars = "Star rating is required";
-    if (stars > 5 || stars < 1)
-      errors.stars = "Star rating must be between 1 and 5! ";
-    setErrors(errors);
+		const errors = {};
+		if (review.length < 10)
+			errors.review = "Review text must be greater than 10 characters.";
+		if (review.length > 250)
+			errors.review = "Review text must be 250 characters or less.";
+		if (!stars) errors.stars = "Star rating is required";
+		if (stars > 5 || stars < 1)
+			errors.stars = "Star rating must be between 1 and 5! ";
+		setErrors(errors);
 
-    if (Object.values(errors).length === 0) {
-      setSubmitted(true);
-      const reviewData = {
-        review,
-        stars,
-      };
+		if (Object.values(errors).length === 0) {
+			setSubmitted(true);
+			const reviewData = {
+				review,
+				stars,
+			};
 
-      try {
-        await dispatch(updateReview(restaurantId, reviewId, reviewData)).then(
-          async () => {
-            await dispatch(fetchReviews());
-            history.push(`/restaurants/${restaurantId}`);
-          }
-        );
-      } catch (error) {
-        console.error("Error creating review:", error);
-        if (error instanceof Response) {
-          const responseJson = await error.json();
-          console.error("Server response:", responseJson);
-        }
-      }
-    }
-  };
+			try {
+				await dispatch(
+					updateReview(restaurantId, reviewId, reviewData)
+				).then(async () => {
+					await dispatch(fetchReviews());
+					history.push(`/restaurants/${restaurantId}`);
+				});
+			} catch (error) {
+				console.error("Error creating review:", error);
+				if (error instanceof Response) {
+					const responseJson = await error.json();
+					console.error("Server response:", responseJson);
+				}
+			}
+		}
+	};
 
-  return (
-    <div className="create-review-container">
-      <h2 className="create-review-title">Alter your review below!</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="review-content-main">
-          <div className="form-row-stars-rating-container">
-            <div className="stars-container">
-              <div className="rating">
-                <div className="stars">
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <span
-                      key={star}
-                      className={`star ${star <= stars ? "filled" : ""}`}
-                      onClick={() => setStars(star)}
-                    >
-                      🌭
-                    </span>
-                  ))}
-                  <span className="stars-text">Select your rating</span>
-                </div>
-              </div>
-              <div className="things-to-consider-container">
-                <span className="things-to-consider-text">
-                  A few things to consider in your review
-                </span>
-                <span className="things-to-consider-categories-food">Food</span>
-                <span className="things-to-consider-categories-service">
-                  Service
-                </span>
-                <span className="things-to-consider-categories-ambience">
-                  Ambience
-                </span>
-              </div>
-            </div>
-            <label className="create-review-label">
-              <textarea
-                type="text"
-                value={review}
-                onChange={(e) => setReview(e.target.value)}
-                className="create-review-input"
-              />
-            </label>
-          </div>
-          <div className="all-errors-reviews">
-            {errors.review && (
-              <div className="create-review-error-div">
-                <span className="create-review-error-text">
-                  ⚠︎ {errors.review}
-                </span>
-              </div>
-            )}
-            {errors.stars && (
-              <div className="create-review-error-div">
-                <span className="create-stars-error-text">
-                  ⚠︎ {errors.stars}
-                </span>
-              </div>
-            )}
-          </div>
-          <br />
-          <button
-            type="submit"
-            className="create-review-submit-button"
-            disabled={submitted}
-          >
-            Update Review
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className="create-review-container">
+			<h2 className="create-review-title">Alter your review below!</h2>
+			<form onSubmit={handleSubmit}>
+				<div className="review-content-main">
+					<div className="form-row-stars-rating-container">
+						<div className="stars-container">
+							<div className="rating">
+								<div className="stars">
+									<span className="stars-text">
+										Select your rating
+									</span>
+									{[5, 4, 3, 2, 1].map((star) => (
+										<div
+											key={star}
+											className={`star ${
+												star <= stars ? "filled" : ""
+											}`}
+											onClick={() => setStars(star)}
+										>
+											<i
+												id="review-star-actl"
+												className="fa-solid fa-star"
+											></i>
+										</div>
+									))}
+								</div>
+							</div>
+							<div className="things-to-consider-container">
+								<span className="things-to-consider-text">
+									A few things to consider in your review
+								</span>
+								<span className="things-to-consider-categories-food">
+									Food
+								</span>
+								<span className="things-to-consider-categories-service">
+									Service
+								</span>
+								<span className="things-to-consider-categories-ambience">
+									Ambience
+								</span>
+							</div>
+						</div>
+						<label className="create-review-label">
+							<textarea
+								type="text"
+								value={review}
+								onChange={(e) => setReview(e.target.value)}
+								className="create-review-input"
+							/>
+						</label>
+					</div>
+					<div className="all-errors-reviews">
+						{errors.review && (
+							<div className="create-review-error-div">
+								<span className="create-review-error-text">
+									⚠︎ {errors.review}
+								</span>
+							</div>
+						)}
+						{errors.stars && (
+							<div className="create-review-error-div">
+								<span className="create-stars-error-text">
+									⚠︎ {errors.stars}
+								</span>
+							</div>
+						)}
+					</div>
+					<br />
+					<button
+						type="submit"
+						className="create-review-submit-button"
+						disabled={submitted}
+					>
+						Update Review
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default UpdateReviewFunc;
